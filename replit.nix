b7bda7aacfc43a5435ed7507cd77b44ee4b0852b
wochub <134020227+wochub@users.noreply.github.com>
2025-05-05 23:19:01 +0000

    Pre-Recovery checkpoint

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodePackages.prettier
+    pkgs.http = "public"
+    pkgs.http = "public"
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server  
+  ];
+}
