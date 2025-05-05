533c97738eae972af9a5a4184ab49fe966df0391
wochub <134020227+wochub@users.noreply.github.com>
2025-05-05 23:19:02 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodePackages.prettier
-    pkgs.http = "public"
-    pkgs.http = "public"
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server  
-  ];
-}
